ececb3805cd3277ad0771bfa6a8acd496c5de371
Deykins <daykat93@gmail.com>
2023-03-31 11:00:48 +0200

    remove 'only' from last test

--- cypress/e2e/rozetka-tests/homepage-elements-tests.cy.js
+++ cypress/e2e/rozetka-tests/homepage-elements-tests.cy.js
@@ -49,10 +49,11 @@ describe('Verifying elements on the homepage of base URL', () => {
         // cy.get('h1').should('have.text', '«phone»')
     })
 
-    it.only('TC000006 Verify, that user can search products', () => {
+    it('TC000006 Verify, that user can search products', () => {
         cy.visit('https://rozetka.pl') 
         cy.get('.action-buttons .button').eq(0).click()
         cy.get('a[class="ng-tns-c109-3"][href*="/about/"]').click()
         cy.url().should('include', '/about/')
     })
+
 })
