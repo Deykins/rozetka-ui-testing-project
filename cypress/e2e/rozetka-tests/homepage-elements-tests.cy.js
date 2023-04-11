37d1f257a8dcb415ff7259fbdd3809bc640f5a0e
Deykins <daykat93@gmail.com>
2023-04-11 13:57:37 +0200

    code debugged, document refactored, mistakes fixed

--- cypress/e2e/rozetka-tests/homepage-elements-tests.cy.js
+++ cypress/e2e/rozetka-tests/homepage-elements-tests.cy.js
@@ -1,12 +1,13 @@
-/// <reference types="Cypress" />
-import Basket_PO from "../../support/pageObjects/rozetka-pl/Basket_PO"
-import ItemPage_PO from "../../support/pageObjects/rozetka-pl/ItemPage_PO"
-import CategoryPage_PO from "../../support/pageObjects/rozetka-pl/CategoryPage_PO"
-import LogInWindow_PO from "../../support/pageObjects/rozetka-pl/LogInWindow_PO"
+import Homepage_PO from "../../support/pageObjects/rozetka-pl/Homepage_PO";
+import Basket_PO from "../../support/pageObjects/rozetka-pl/Basket_PO";
+import ItemPage_PO from "../../support/pageObjects/rozetka-pl/ItemPage_PO";
+import CategoryPage_PO from "../../support/pageObjects/rozetka-pl/CategoryPage_PO";
+import LogInWindow_PO from "../../support/pageObjects/rozetka-pl/LogInWindow_PO";
 
+/// <reference types="Cypress" />
 
 describe("Verifying elements on the homepage of base URL", () => {
-  const homepage = new HomePage_PO();
+  const homepage = new Homepage_PO();
   const basket = new Basket_PO();
   const itemPage = new ItemPage_PO();
   const categoryPage = new CategoryPage_PO();
@@ -14,9 +15,8 @@ describe("Verifying elements on the homepage of base URL", () => {
 
   beforeEach(() => {
     homepage.visitHomepage({ timeout: 10000 });
-    homepage.acceptCookies()
+    homepage.acceptCookies();
     cy.fixture("rozetkaTestData").as("data");
-    
   });
 
   it("TC000001 Verify, that homepage is displayed", () => {
@@ -38,7 +38,7 @@ describe("Verifying elements on the homepage of base URL", () => {
       expectedText = $cat.text();
     });
     homepage.getCategory(0).click({ force: true });
-    categoryPage.getCategoryTitle({timeout:5000}).then(($title) => {
+    categoryPage.getCategoryTitle({ timeout: 5000 }).then(($title) => {
       expect($title.text()).to.equal(expectedText);
     });
   });
@@ -76,13 +76,13 @@ describe("Verifying elements on the homepage of base URL", () => {
     });
   });
 
-  it("TC000008 Verify, that correct error message displayed on login form", () => {
+  it("TC000009 Verify, that registered user can login with valid credentials", () => {
     homepage.logIn();
     cy.get("@data").then((data) => {
       logInWindow.getEmailField().type(data.email);
       logInWindow.getPasswordField().type(data.password);
     });
-    logInWindow.getRememberMeCheckbox().ch
+    logInWindow.getRememberMeCheckbox().ch;
     logInWindow.getSubmitButton().click();
     cy.get("#ngrecaptcha-0 *> iframe").should("be.visible");
     // cy.get('#ngrecaptcha-0 *> iframe').then($iframe => {
@@ -92,40 +92,11 @@ describe("Verifying elements on the homepage of base URL", () => {
     // cy.get('@iframe').find('#rc-anchor-container')
   });
 
-    it('TC000007 Verify "O nas" link is redirect to correct page', () => {
-        homepage.getAboutUsLink().click()
-        cy.url().should('include', '/about/')
-    })
-
-    it('TC000008 Verify, that correct error message displayed on login form', () => {
-        homepage.logIn()
-        logInWindow.getSubmitButton().click()
-        cy.get('@data').then((data) => {
-            logInWindow.getErrortMessage().should('have.text', data.errorMessage)
-        })
-    })
-
-    it('TC000009 Verify, that registered user can login with valid credentials', () => {
-        homepage.logIn()
-        cy.get('@data').then((data) => {
-            logInWindow.getEmailField().type(data.email)
-            logInWindow.getPasswordField().type(data.password)
-        })
-        logInWindow.getRememberMeCheckbox().uncheck({force:true})
-        logInWindow.getSubmitButton().click()
-        cy.get('#ngrecaptcha-0 *> iframe').should('be.visible')
-        // cy.get('#ngrecaptcha-0 *> iframe').then($iframe => {
-        //     const body = $iframe.contents().find('body')
-        //     cy.wrap(body).as('iframe')
-        // })
-        // cy.get('@iframe').find('#rc-anchor-container')
-    })
-
-    it("TC000010 Verify, that categories displayed in correct order", () => {
-        cy.get('@data').then(data => {
-            homepage.getCategoryList().each(($el, index, $list) => {
-                expect($el.text()).is.equal(data.categoryList[index])  //Check the list
-            })
-        })
-    })
-})
+  it("TC000010 Verify, that categories displayed in correct order", () => {
+    cy.get("@data").then((data) => {
+      homepage.getCategoryList().each(($el, index, $list) => {
+        expect($el.text()).is.equal(data.categoryList[index]); //Check the list
+      });
+    });
+  });
+});
